4a0814f837d193af7096b4c0706554585bd9375c
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-30 20:00:15 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,16 @@ public class PracticeProblem {
 
 	}
 
+	public static void insertionSort(char[] characters){
+		for (int j = 1;j < characters.length;j++){
+			char key = characters[j];
+			int index = j-1;
+			while (index >= 0 && key < characters[index]){
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+		}
+		}
+	}
 
-}
